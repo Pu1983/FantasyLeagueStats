9a29ef362f240f454be972017b6bb6af4a8b5435
hannes.neubert <hannes.neubert@kaufda.de>
2025-11-06 15:06:08 +0100

    fix password

--- fantasystats/fantasystats/settings.py
+++ fantasystats/fantasystats/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)9pi4p+k_d+(lo+*4ywoh9^y+34cf=xhdfiu1#m&#-173vbv_q"
+SECRET_KEY = "django-insecure-)9pi4p+k_d+(lo+*4ywoh9^y+34cf=xhdfiu1#m&#-173vbv_qrst"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
